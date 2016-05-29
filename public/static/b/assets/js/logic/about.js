b5a8fc77391cfa188beb7ed6455e8e214a846dee
boris_plotnikov <pborisp@gmail.com>
2016-05-29 02:15:24 +0200

    movement date time picker

--- public/static/b/assets/js/logic/about.js
+++ public/static/b/assets/js/logic/about.js
@@ -9,23 +9,28 @@ about.prototype.constructor = about;
 
 //overriding page init
 about.prototype.init = function() {
+    self = this;
     l("in about init function");
     this.firstRun = true;
     this.animationSpeed = 1000;
     this.basePath = "http://percolator.modcam.io/stitch/vamuseum/";
-    this.currentDate = new Date(2016, 4, 17, 6, 0, 0, 0);
+
+    //using date and time from the time picker
+    //this.currentDate = new Date(2016, 4, 17, 6, 0, 0, 0);
 
     d3.select('#slider1').call(
         d3.slider().step(1).value(speed).on("slide", function(evt, value) {
             speed = value;
         }));
 
+    this.initDateTimePicker();
 
     //var path = "http://percolator.modcam.io/stitch/vamuseum/2016-05-16%2016:00/2016-05-16%2023:00?wp=5000";
     this.container = $('.movement-img-container');
     //pc.pageScript.timeOut = setTimeout(pc.pageScript.animateLoop, this.animationSpeed);
 
-    this.animateLoop();
+    //this.animateLoop();
+    $('#runSimulation').click(self.runSimulation);
 };
 //overriding page destruct
 about.prototype.destruct = function() {
@@ -47,8 +52,48 @@ about.prototype.setDateTime = function (d, delay ){
     startTime += d.getUTCMinutes();
     return startTime;
 }
+about.prototype.initDateTimePicker = function(){
+    var self = this;
+    self.dtPick = $('#datetimepicker');
+    self.dtPick.datetimepicker({
+        onChangeDateTime:self.dateTimeAction,
+        onShow:self.dateTimeAction,
+        minDate:'2016/05/17',
+        maxDate:'2016/12/31',
+        yearStart:'2016',
+        yearEnd:'2017',
+        step:15,
+        value:'2016/05/17 10:00',
+        //onChangeDateTime:function(dp,$input){
+        //  alert($input.val());}
+    });
+    $('#dateTimeClick').click(function(){
+        self.dtPick.datetimepicker('show');
+    })
+    self.currentDate = self.dtPick.datetimepicker('getValue');
+};
+//init the date picker
+about.prototype.dateTimeAction = function( currentDateTime,$input ) {
+    console.log("sdf");
+    console.log(currentDateTime);
+    //var d = $('#input').datetimepicker('getValue');
+    var d = $input.datetimepicker('getValue');
+    if (d != null) {
+        console.log(d.getTime() / 1000);
+        pc.pageScript.currentDate = pc.pageScript.dtPick.datetimepicker('getValue');
+    }
+  //  alert($input.val());
+    // 'this' is jquery object datetimepicker
+    //this.setOptions({
+    //     minTime:'8:00'
+    //  });
+};
+about.prototype.runSimulation = function () {
+    console.log("running simulation");
+    pc.pageScript.animateLoop();
+}
 about.prototype.animateLoop = function () {
-    //setTimeout chages the scope so this is the window in the second run
+    //setTimeout changes the scope so this is the window in the second run
     // so for now just calling the current script from the page controller (maybe this isn't the best option,
     // or if it is should be more consistent
     l("animating map");
